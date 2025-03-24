49cab80e0f7d67814b93016eab22b54d5667953d
Sapienti <sapienti17@gmail.com>
2025-03-24 07:37:49 +0300

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-const element = document.querySelector(".container");
-console.log(element);
-
-const elementContext = element.innerHTML;
-console.log(elementContext);
-
-const test = document.body;
-// console.log(test);
